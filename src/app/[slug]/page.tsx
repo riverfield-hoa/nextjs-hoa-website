cd2fc4c3d4ccc568582385a87d0b904e272ffb54
Danny Shay <danny624@gmail.com>
2025-01-22 10:43:41 -0500

    Initial commit

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,52 @@
+import { PortableText, type SanityDocument } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { client } from "@/sanity/client";
+import Link from "next/link";
+import { Image } from "next-sanity/image";
+
+const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
+
+const { projectId, dataset } = client.config();
+const urlFor = (source: SanityImageSource) =>
+    projectId && dataset
+        ? imageUrlBuilder({ projectId, dataset }).image(source)
+        : null;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function PostPage(props: { params: Promise<{ slug: string[] }> }) {
+    const post = await client.fetch<SanityDocument>(POST_QUERY, props.params, options);
+    const postImageUrl = post.image
+        ? urlFor(post.image)?.width(550).height(310).url()
+        : null;
+
+    return (
+        <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
+            <Link href="/" className="hover:underline">
+                ← Back to posts
+            </Link>
+            {postImageUrl && (
+                <Image
+                    src={postImageUrl}
+                    alt={post.title}
+                    className="aspect-video rounded-xl"
+                    width="550"
+                    height="310"
+                />
+            )}
+            <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+            <div className="prose">
+                <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
+                {Array.isArray(post.body) && <PortableText value={post.body} />}
+            </div>
+        </main>
+    );
+}
+
+export async function generateStaticParams() {
+    const posts = await client.fetch<SanityDocument[]>(
+        `*[_type == "post"]{ "slug": slug.current }`
+    );
+    return posts.map((post) => ({ slug: post.slug }));
+}
